1bee62cd62844bf646fdbb49396d8c9672a4fa32
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-11 00:37:25 +0200

    adding a comment -> server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public')); // serve per i file statici (HTML, CSS, JS) nella cartella 'public'
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
